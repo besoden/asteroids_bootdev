e4625b2eaa97af9b725511944be5754dc808a555
besoden <nive.parthiban@gmail.com>
2025-06-01 18:07:16 -0700

    player can rotate right or left with a d or right left keys

--- player.py
+++ player.py
@@ -17,3 +17,18 @@ class Player(circleshape.CircleShape):
     
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
+    
+    def rotate(self, dt):
+        self.rotation += constants.PLAYER_TURN_SPEED * dt
+    
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(dt * -1) #rotate left
+        if keys[pygame.K_d]:
+            self.rotate(dt) #rotate right
+        if keys[pygame.K_LEFT]:
+            self.rotate(dt * -1) #rotate left
+        if keys[pygame.K_RIGHT]:
+            self.rotate(dt) #rotate right
