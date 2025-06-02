acf4457a99dd9adf9b07b851b8ba390ff193cc28
besoden <nive.parthiban@gmail.com>
2025-06-01 19:46:26 -0700

    moved player object to groups

--- main.py
+++ main.py
@@ -11,15 +11,22 @@ def main():
     print(f"Screen width: {constants.SCREEN_WIDTH}")
     print(f"Screen height: {constants.SCREEN_HEIGHT}")
 
+    updatables = pygame.sprite.Group()
+    drawables = pygame.sprite.Group()
+    Player.containers = (updatables, drawables)
     player = Player(constants.SCREEN_WIDTH/2, constants.SCREEN_HEIGHT/2)
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill(color="black")
-        player.update(dt)
-        player.draw(screen)
+        
+        updatables.update(dt)
+        for drawable in drawables:
+            drawable.draw(screen) 
+
         pygame.display.flip()
         dt = clock.tick(60)/1000
 
