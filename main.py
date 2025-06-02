f7339c8481f0aeecb5a4f4f1f8b52ddaa7075d66
besoden <nive.parthiban@gmail.com>
2025-06-01 17:55:47 -0700

    draws player in middle of screen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import constants
+from player import Player
 
 def main():
     pygame.init()
@@ -10,12 +11,14 @@ def main():
     print(f"Screen width: {constants.SCREEN_WIDTH}")
     print(f"Screen height: {constants.SCREEN_HEIGHT}")
 
+    player = Player(constants.SCREEN_WIDTH/2, constants.SCREEN_HEIGHT/2)
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(color="gray")
+        screen.fill(color="black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)/1000
 
